66393fdcb871adac59957573bfa8c6ecb69fd5ef
PRANEETH REDDY <108972019+praneethsattavaram@users.noreply.github.com>
2024-02-26 12:39:09 +0530

    Add files via upload

---
+++ libraryinfo/libraryinfo/libraryinfo_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def home(request):
+    return render(request,"index.html",context={})
+
+def shopping(request):
+    return HttpResponse("Welcome to Shopping")
+
+def save_student(request):
+    student_name = request.POST['student_name']
+    return render(request,"welcome.html",context={'student_name':student_name})
